790a916da0a681707653f5535f84d8a57f48751f
hm123777 <hm123777@gmail.com>
2019-02-24 14:36:54 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,20 @@ class UsersController < ApplicationController
   def index
     @users = User.all.page(params[:page])
   end
-
+  
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order('created_at DESC').page(params[:page])
+    counts(@user)
   end
-
+  
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
-  
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -24,11 +26,11 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
-  
+
 end
